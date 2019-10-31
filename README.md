9923661f8c96651934ce88e391d864e1f2a69a7c
chdq <hh934609672@163.com>
2019-10-31 18:34:29 +0800

    change readme file

--- README.md
+++ README.md
@@ -1,1 +1,5 @@
 # openGl study
+--
+## example
+>example 包下
+1.  DrawTriangle_1 画三角形（利用shader）
--- example/DrawTriangle_1.cpp
+++ example/DrawTriangle_1.cpp
@@ -8,29 +8,48 @@
 using namespace std;
 
 void DrawTriangle::doRender() {
+    glPolygonMode(GL_FRONT_AND_BACK, GL_FILL);//设置绘制模式，填充或者划线GL_LINE
     glUseProgram(shaderProgram);
     glBindVertexArray(VAO);
-    glDrawArrays(GL_TRIANGLES, 0, 3);
+//    glDrawArrays(GL_TRIANGLES, 0, 3); //画三角形
+//EBO不能单独使用必须和VBO配合使用，VBO可以单独使用
+    glDrawElements(GL_TRIANGLES, 6, GL_UNSIGNED_INT, 0);//画正方形，当使用索引缓冲对象的时候，最后一个参数为0，否则是一个索引数组
+//    glBindVertexArray(0);
 }
 
 void DrawTriangle::beforeRender() {
-    float vertices[] = {
-            -0.5f, -0.5f, 0.0f,
-            0.5f, -0.5f, 0.0f,
-            0.0f, 0.5f, 0.0f
+//    float vertices[] = { 绘制三角形
+//            -0.5f, -0.5f, 0.0f,
+//            0.5f, -0.5f, 0.0f,
+//            0.0f, 0.5f, 0.0f
+//    };
+    float vertices[] = {//绘制正方形
+            0.5f, 0.5f, 0.0f,   // 右上角
+            0.5f, -0.5f, 0.0f,  // 右下角
+            -0.5f, -0.5f, 0.0f, // 左下角
+            -0.5f, 0.5f, 0.0f   // 左上角
     };
-    unsigned int VBO;
+
+    unsigned int indices[] = { // 注意索引从0开始!
+            0, 1, 3, // 第一个三角形
+            1, 2, 3  // 第二个三角形
+    };
     glGenVertexArrays(1, &VAO);
+    unsigned int VBO;
     glGenBuffers(1, &VBO);//生成缓冲对象
+    unsigned int EBO;
+    glGenBuffers(1, &EBO);
     glBindVertexArray(VAO);
     glBindBuffer(GL_ARRAY_BUFFER, VBO);//绑定缓冲区
+    glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, EBO);//绑定缓冲区
 
     /**
      * GL_STATIC_DRAW ：数据不会或几乎不会改变。
      * GL_DYNAMIC_DRAW：数据会被改变很多。
      * GL_STREAM_DRAW ：数据每次绘制时都会改变。
      */
-    glBufferData(GL_ARRAY_BUFFER, sizeof(vertices), vertices, GL_STATIC_DRAW);//将顶点复制到缓冲区，第二个参数以字节为单位
+    glBufferData(GL_ARRAY_BUFFER, sizeof(vertices), vertices, GL_STATIC_DRAW);//将顶点复制到缓冲区，第二个参数以字节为单位,三角形
+    glBufferData(GL_ELEMENT_ARRAY_BUFFER, sizeof(indices), indices, GL_STATIC_DRAW);//将顶点复制到缓冲区，第二个参数以字节为单位，正方形
     unsigned int vertexShader;
     vertexShader = glCreateShader(GL_VERTEX_SHADER);//创建顶点着色器
     char *vertexShaderSource = "#version 330 core\n"
@@ -84,4 +103,5 @@ void DrawTriangle::beforeRender() {
 
     glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 3 * sizeof(float), (void *) 0);
     glEnableVertexAttribArray(0);
+    glBindVertexArray(0);
 }
